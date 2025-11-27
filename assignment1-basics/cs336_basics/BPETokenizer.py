fe0841614eecc23d410664ded796c181fb45b774
Homan <homanhan@outlook.com>
2025-11-27 21:07:13 +0800

    Finish BPE Training

--- assignment1-basics/cs336_basics/BPETokenizer.py
+++ assignment1-basics/cs336_basics/BPETokenizer.py
@@ -22,7 +22,9 @@ def find_chunk_boundaries(
     Chunk the file into parts that can be counted independently.
     May return fewer chunks if the boundaries end up overlapping.
     """
-    assert isinstance(split_special_token, bytes), "Must represent special token as a bytestring"
+    assert isinstance(
+        split_special_token, bytes
+    ), "Must represent special token as a bytestring"
 
     # Get total file size in bytes
     file.seek(0, os.SEEK_END)
@@ -59,6 +61,7 @@ def find_chunk_boundaries(
     # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
     return sorted(set(chunk_boundaries))
 
+
 # ## Usage
 # with open(..., "rb") as f:
 #     num_processes = 4
@@ -70,6 +73,7 @@ def find_chunk_boundaries(
 #         f.seek(start)
 #         chunk = f.read(end - start).decode("utf-8", errors="ignore")
 #         # Run pre-tokenization on your chunk and store the counts for each pre-token
+
 
 def split_special_token(
     chunk: str,
@@ -92,9 +96,10 @@ def split_special_token(
         pattern = "(" + "|".join(re.escape(token) for token in special_token) + ")"
         return re.split(pattern, chunk)
 
+
 def pre_tokenize_chunk(
     chunk: str,
-    special_tokens: list[str] = None,
+    special_tokens: list[str] = [],
     drop_special_tokens: bool = True,
 ) -> list[bytes]:
     """
@@ -103,9 +108,11 @@ def pre_tokenize_chunk(
     """
     # 4.1 split by special tokens
     parts = split_special_token(chunk, special_tokens)
-    
+
     # 4.2 count tokens in non-special-token parts
-    PATTERN = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+    PATTERN = (
+        r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+    )
     token_counts = []
     for part in parts:
         if part in special_tokens:
@@ -120,6 +127,7 @@ def pre_tokenize_chunk(
                 token_counts.append(token_bytes)
     return token_counts
 
+
 def worker(
     chunk: str,
     special_tokens: list[str],
@@ -132,11 +140,67 @@ def worker(
     queue.put(token_counts)
 
 
+# From https://github.com/Spectual/stanford-cs336-a1/blob/main/cs336_basics/BPETokenizer.py#L197
+def merge(
+    counts: dict[tuple[int, int], int],
+    counts_idx: dict[tuple[int, int], set[int]],
+    pre_tokens: list[list[int]],
+    best_pair: tuple[int, int],
+    new_index: int,
+) -> None:
+    """Merge the pairs with highest frequency and update counts, counts_idx"""
+    index_set = counts_idx[best_pair]
+
+    for i in index_set:
+        pretoken = pre_tokens[i]
+        new_pretoken = []
+
+        pos_list = []  # Store positions of best_pair for each new pretoken after merge
+        pos = 0
+        j = 0
+
+        # Replace best_pair with new_index in each pretoken
+        while j < len(pretoken):
+            if (j < len(pretoken) - 1) and (
+                (pretoken[j], pretoken[j + 1]) == best_pair
+            ):
+                new_pretoken.append(new_index)
+                pos_list.append(pos)
+                j += 2
+            else:
+                new_pretoken.append(pretoken[j])
+                j += 1
+            pos += 1
+
+        # Update counts and counts_idx
+        for pos in pos_list:
+            counts[best_pair] -= 1
+
+            if pos > 0:
+                if new_pretoken[pos - 1] == new_index:
+                    counts[(best_pair[1], best_pair[0])] -= 1
+                else:
+                    counts[(new_pretoken[pos - 1], best_pair[0])] -= 1
+
+                counts[(new_pretoken[pos - 1], new_pretoken[pos])] += 1
+                counts_idx[(new_pretoken[pos - 1], new_pretoken[pos])].add(i)
+
+            if pos < len(new_pretoken) - 1:
+                if new_pretoken[pos + 1] == new_index:
+                    counts[(best_pair[1], best_pair[0])] -= 1
+                else:
+                    counts[(best_pair[1], new_pretoken[pos + 1])] -= 1
+
+                counts[(new_pretoken[pos], new_pretoken[pos + 1])] += 1
+                counts_idx[(new_pretoken[pos], new_pretoken[pos + 1])].add(i)
+
+        pre_tokens[i] = new_pretoken
+
 
 def train_bpe(
     intput_path: str,
     vocab_size: int,
-    special_tokens: list[str] = None,
+    special_tokens: list[str] = [],
 ) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
     """
     Train a BPE tokenizer on the input file.
@@ -149,40 +213,44 @@ def train_bpe(
     5. train BPE on the pre-tokenized chunks
     6. return the vocabulary and merges
     """
-    
+
     vocab = {}
     merges = []
 
     # 1. init vocab
-    vocab = {x:bytes([x]) for x in range(256)}
+    vocab = {x: bytes([x]) for x in range(256)}
     # tips: b"a" 是 bytes 的字面量. 只允许
     #       bytes(x) 是 x 个 0x00 字节组成的 bytes
     #       bytes([x]) 将 ID (0 <= x <= 255) 转换为单字节的 bytes
     # example: >>> bytes([65]) == b"A"
     #          True
-    
+
     # 2. add special tokens to vocab
     special_tokens = special_tokens or []
     for i, token in enumerate(special_tokens):
         vocab[256 + i] = token.encode("utf-8")
-    
+
     num_processes = 4
     chunks = []
     # 3. read the file and find chunk boundaries
-    with open(..., "rb") as f:
+    with open(intput_path, "rb") as f:
         boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
 
         # The following is a serial implementation, but you can parallelize this
         # by sending each start/end pair to a set of processes.
-        for start, end in zip(boundaries[:-1], boundaries[1:]): # (b[0], b[1]), (b[1], b[2])...
+        for start, end in zip(
+            boundaries[:-1], boundaries[1:]
+        ):  # (b[0], b[1]), (b[1], b[2])...
             f.seek(start)
-            chunk = f.read(end - start).decode("utf-8", errors="ignore") # f.read() returns bytes, decode to str
+            chunk = f.read(end - start).decode(
+                "utf-8", errors="ignore"
+            )  # f.read() returns bytes, decode to str
             # Run pre-tokenization on your chunk and store the counts for each pre-token
             chunks.append(chunk)
 
     # 4. pre-tokenize the chunks (remove special tokens) parallelized
     processes = []
-    pre_tokens = []
+    pre_tokens = []  # final result of stage 4 (pre-tokenized tokens)
     q = Queue()
     for chunk in chunks:
         p = Process(target=worker, args=(chunk, special_tokens, q))
@@ -191,54 +259,44 @@ def train_bpe(
 
     # Collect pre-tokenization results from all processes
     pre_token_lists = [q.get() for _ in processes]
-    
+
     for p in processes:
         p.join()
-    
+
     pre_tokens = [token for sublist in pre_token_lists for token in sublist]
 
     # 5. train BPE on the pre-tokenized chunks
     # 5.1 全局统计一次 bytes pair 出现次数
-    counts = defaultdict(int)
-    counts_idx = defaultdict(set)
+    counts = defaultdict(int)  # 统计不同 bytes pair 出现的次数
+    counts_idx = defaultdict(set)  # 统计不同 bytes pair 出现在哪些单词中
 
     for i, token in enumerate(pre_tokens):
         for token1, token2 in zip(token[:-1], token[1:]):
-            counts[(token1, token2)] += 1 # {(int, int): int}
-            counts_idx[(token1, token2)].add(i) # {(int, int): set(int)} # 记录在哪个单词中，要不要记录在单词中的偏移？
+            counts[(token1, token2)] += 1  # {(int, int): int}
+            counts_idx[(token1, token2)].add(
+                i
+            )  # {(int, int): set(int)} # 记录在哪个单词中，要不要记录在单词中的偏移？
 
     while len(vocab) < vocab_size:
         if not counts:
             break
-        
+
         # 5.2 找到出现次数最多的 bytes pair。如果次数相同，选择字典序最大的合并
-        best_pair = max(    # 使用最大堆维护最大值？
+        best_pair = max(  # 使用最大堆维护最大值？
             counts.items(),
             key=lambda x: (
                 x[1],
                 vocab[x[0][0]].decode("utf-8", errors="ignore"),
-                vocab[x[0][1]].decode("utf-8", errors="ignore")
-            )
+                vocab[x[0][1]].decode("utf-8", errors="ignore"),
+            ),
         )[0]
 
         # 5.3 将该 bytes pair 合并为一个新的 token，加入 vocab
         new_token = vocab[best_pair[0]] + vocab[best_pair[1]]
         vocab[len(vocab)] = new_token
-        merges.append(best_pair)
+        merges.append((vocab[best_pair[0]], vocab[best_pair[1]]))
 
         # 5.4 更新 pre_tokens 和 counts 频率
-        merge(best_pair, new_token, pre_tokens, counts, counts_idx, vocab)
+        merge(counts, counts_idx, pre_tokens, best_pair, len(vocab) - 1)
 
-    
-
-
-    
     return vocab, merges
-
-
-
-
-
-
-
-
